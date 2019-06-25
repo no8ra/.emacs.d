a1dbea00f3a52b94a27c203677b4e4ca1ac3dbad
yagi <hiro.yagi0245@gmail.com>
2019-06-25 16:27:28 +0900

    rm stable

--- init.el
+++ init.el
@@ -39,7 +39,6 @@
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
 	("melpa" . "http://melpa.org/packages/")
-	;; ("melpa-stable" . "https://stable.melpa.org/packages/")
   	("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 (unless package-archive-contents
