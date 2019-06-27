bc64f3e6d8ba5682f39efee13719cfbf862825ca
hiroki yagi <hiro.yagi0245@gmail.com>
2019-06-27 11:10:50 +0900

    ?

--- init.el
+++ init.el
@@ -399,6 +399,15 @@
 (add-to-list 'auto-insert-alist
 	     '("\\.scm" .
 	       (insert "#!/bin/sh\n#| -*- scheme -*-\nexec csi -s $0 \"$@\"\n|#\n")))
+;; Indenting module body code at column 0
+(defun scheme-module-indent (state indent-point normal-indent) 0)
+(put 'module 'scheme-indent-function 'scheme-module-indent)
+(put 'and-let* 'scheme-indent-function 1)
+(put 'parameterize 'scheme-indent-function 1)
+(put 'handle-exceptions 'scheme-indent-function 1)
+(put 'when 'scheme-indent-function 1)
+(put 'unless 'scheme-indent-function 1)
+(put 'match 'scheme-indent-function 1)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; GEISER
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -440,7 +449,8 @@
   (add-hook 'inferior-ess-mode-hook
 	    (lambda ()
 	      (my-ess-hook)))
-  (define-key ess-r-mode-map "_" #'ess-insert-assign))
+  (define-key ess-r-mode-map "_" #'ess-insert-assign)
+  (define-key inferior-ess-r-mode-map "_" #'ess-insert-assign))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Markdown
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
