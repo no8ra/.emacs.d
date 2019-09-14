be506f6e54933089603a1a35d8f941383b350d66
hiroki yagi <hiro.yagi0245@gmail.com>
2019-09-14 15:05:57 +0900

    fix google-translate

--- init.el
+++ init.el
@@ -925,30 +925,32 @@
 (use-package google-translate
   :ensure t
   :bind
-  (("s-g" . google-translate-at-point))
+  (("s-g" . google-translate-enja-or-jaen))
   :config
-  (require 'google-translate-smooth-ui))
-;; (defvar google-translate-english-chars "[:ascii:]"
-;;   "これらの文字が含まれているときは英語とみなす")
-;; (defun google-translate-enja-or-jaen (&optional string)
-;;   "regionか、現在の単語を言語自動判別でGoogle翻訳する。"
-;;   (interactive)
-;;   (setq string
-;;         (cond ((stringp string) string)
-;;               (current-prefix-arg
-;;                (read-string "Google Translate: "))
-;;               ((use-region-p)
-;;                (buffer-substring (region-beginning) (region-end)))
-;;               (t
-;;                (current-word))))
-;;   (let* ((asciip (string-match
-;;                   (format "\\`[%s]+\\'" google-translate-english-chars)
-;;                   string)))
-;;     (run-at-time 0.1 nil 'deactivate-mark)
-;;     (google-translate-translate
-;;      (if asciip "en" "ja")
-;;      (if asciip "ja" "en")
-;;      string)))
+  (require 'google-translate-smooth-ui)
+  (setq google-translate-backend-method 'curl)
+  (defvar google-translate-english-chars "[:ascii:]"
+    "これらの文字が含まれているときは英語とみなす")
+  (defun google-translate-enja-or-jaen (&optional string)
+    "regionか、現在の単語を言語自動判別でGoogle翻訳する。"
+    (interactive)
+    (setq string
+          (cond ((stringp string) string)
+		(current-prefix-arg
+		 (read-string "Google Translate: "))
+		((use-region-p)
+		 (buffer-substring (region-beginning) (region-end)))
+		(t
+		 (current-word))))
+    (let* ((asciip (string-match
+                    (format "\\`[%s]+\\'" google-translate-english-chars)
+                    string)))
+      (run-at-time 0.1 nil 'deactivate-mark)
+      (google-translate-translate
+       (if asciip "en" "ja")
+       (if asciip "ja" "en")
+       string))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Tree View
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -1037,4 +1039,3 @@
   :config
   (add-hook 'dart-mode-hook
 	    (lsp-deferred)))
-
